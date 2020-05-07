2dd12eafd000e84c5c4ee69ecc13ee6e0ac99ab2
hamza-aabila <45084155+hamza-aabila@users.noreply.github.com>
2020-05-07 12:13:56 +0000

    Add Camera to SQLite

--- src/app/database/database.page.ts
+++ src/app/database/database.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 import { Platform } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-database',
@@ -8,14 +9,20 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['./database.page.scss'],
 })
 export class DatabasePage implements OnInit {
-
+  capturedSnapURL:string;
+  cameraOptions: CameraOptions = {
+    quality: 20,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
   databaseObj:SQLiteObject;
   name_model:string="";
   row_data:any=[];
   readonly database_name:string = "licda_datatable.db";
   readonly table_name:string = "licdatable";
 
-  constructor(private platform:Platform, private sqlite:SQLite) {
+  constructor(private platform:Platform, private sqlite:SQLite, private camera:Camera) {
     this.platform.ready().then(() => {
       this.createDB();
     }).catch(error=>{
@@ -40,7 +47,7 @@ export class DatabasePage implements OnInit {
 
   createTable() {
     this.databaseObj.executeSql('CREATE TABLE IF NOT EXISTS ' +
-    this.table_name + ' (pid INTEGER PRIMARY KEY, Name varchar(255))',
+    this.table_name + ' (pid INTEGER PRIMARY KEY, Name varchar(255), Image varchar(255))',
     []).then(() => {
     alert('Table Created!');
     }).catch(e => { alert("error " + JSON.stringify(e));
@@ -52,8 +59,7 @@ export class DatabasePage implements OnInit {
     alert("Enter Name");
     return;
     }
-    this.databaseObj.executeSql(`INSERT INTO ${this.table_name} (Name) VALUES ('${this.name_model}')
-  `, [])
+    this.databaseObj.executeSql(`INSERT INTO ${this.table_name} (Name, Image) VALUES ('${this.name_model}', '${this.capturedSnapURL}')`, [])
     .then(() => {
     alert('Row Inserted!');
     this.getRows();
@@ -78,6 +84,16 @@ export class DatabasePage implements OnInit {
     .then((res) => { alert("Row Deleted!"); this.getRows(); })
     .catch(e => { alert("error " + JSON.stringify(e)) }); 
   }  
+
+  CapturePhoto() {
+    this.camera.getPicture(this.cameraOptions).then((imageData) => {
+    let base64Image = 'data:image/jpeg;base64,' + imageData;
+    this.capturedSnapURL = base64Image;
+    }, (err) => {
+    console.log(err);
+    // Handle error
+    });
+  }
 
 }
 
